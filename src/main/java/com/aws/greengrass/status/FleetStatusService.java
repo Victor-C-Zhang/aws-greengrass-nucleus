3491472f8e013817a903882a2a96f4e2c8c7a914
fahadmohammed01 <52720275+fahadmohammed01@users.noreply.github.com>
2021-05-24 15:55:39 -0700

    feat(deployment): remove components when device is removed from a thing group (#977)

--- src/main/java/com/aws/greengrass/status/FleetStatusService.java
+++ src/main/java/com/aws/greengrass/status/FleetStatusService.java
@@ -404,13 +404,15 @@ public class FleetStatusService extends GreengrassService {
                 }
                 List<String> componentGroups = new ArrayList<>();
                 if (finalComponentsToGroupsTopics != null) {
-                    Topics groupsTopics = finalComponentsToGroupsTopics.lookupTopics(service.getName());
-                    groupsTopics.children.values().stream().map(n -> (Topic) n).map(Topic::getName)
-                            .forEach(groupName -> {
-                                componentGroups.add(groupName);
-                                // Get all the group names from the user components.
-                                allGroups.add(groupName);
-                            });
+                    Topics groupsTopics = finalComponentsToGroupsTopics.findTopics(service.getName());
+                    if (groupsTopics != null) {
+                        groupsTopics.children.values().stream().map(n -> (Topic) n).map(Topic::getName)
+                                .forEach(groupName -> {
+                                    componentGroups.add(groupName);
+                                    // Get all the group names from the user components.
+                                    allGroups.add(groupName);
+                                });
+                    }
                 }
                 Topic versionTopic = service.getServiceConfig().findLeafChild(KernelConfigResolver.VERSION_CONFIG_KEY);
                 ComponentStatusDetails componentStatusDetails = ComponentStatusDetails.builder()
@@ -452,6 +454,7 @@ public class FleetStatusService extends GreengrassService {
                 .sequenceNumber(sequenceNumber)
                 .deploymentInformation(deploymentInformation)
                 .build();
+
         publisher.publish(fleetStatusDetails, components);
         logger.atInfo().event("fss-status-update-published").log("Status update published to FSS");
     }
